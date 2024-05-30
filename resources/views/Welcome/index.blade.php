bc1cbf00bcb8d928d341146f41c9d560b2191eaf
airjuice1 <airjuice1@gmail.com>
2024-05-30 13:37:38 +0300

    change var names in welcome view loops

--- resources/views/Welcome/index.blade.php
+++ resources/views/Welcome/index.blade.php
@@ -18,28 +18,26 @@
 				@csrf
 
 				<div class="col">
-					<button type="submit" class="btn btn-primary mb-3 form-control-lg">{{ __('Найти')}}</button>
+					<button type="submit" class="btn btn-primary mb-3 form-control-lg">{{ __('Найти') }}</button>
 				</div>
 
 				<h4>{{ __('Модель') }}</h4>
 				<select class="form-select" multiple aria-label="Multiple select example" name="model_id[]">
-					@foreach ($models as $m)
-						<option value="{{ $m['id'] }}">{{ $m['name'] }}</option>
+					@foreach ($models as $model)
+						<option value="{{ $model['id'] }}">{{ $model['name'] }}</option>
 					@endforeach
 				</select>
-
-				<br>
-
-				@foreach ($params as $p)
+				
+				@foreach ($params as $param)
 					@php
 						$name = false;
-						if ($p['id'] == 1) $name = 'color_id[]';
-						if ($p['id'] == 2) $name = 'size_id[]';
+						if ($param['id'] == 1) $name = 'color_id[]';
+						if ($param['id'] == 2) $name = 'size_id[]';
 					@endphp
-					<h4>{{ __($p['name']) }}</h4>
+					<h4>{{ __($param['name']) }}</h4>
 					<select class="form-select" multiple aria-label="Multiple select example" name="{{ $name }}">
-						@foreach ($p['options'] as $o)
-							<option value="{{ $o['id'] }}">{{ __($o['value']) }}</option>
+						@foreach ($param['options'] as $option)
+							<option value="{{ $option['id'] }}">{{ __($option['value']) }}</option>
 						@endforeach
 					</select>
 				@endforeach
